96d9bbd8a075cdd4c67664fc3552cb5877268fe1
B1GGersnow <2642448440@qq.com>
2022-10-04 13:46:46 +0800

    提交1.0版本文件

--- app/src/main/java/cc/liyaya/mylove/tool/StringUtil.java
+++
@@ -1,9 +0,0 @@
-package cc.liyaya.mylove.tool;
-
-public class StringUtil {
-    public static String append2(String s){
-        String regex = "(.{2})";
-        s = s.replaceAll(regex,"$1\n");
-        return s.substring(0,s.length() - 1);
-    }
-}
